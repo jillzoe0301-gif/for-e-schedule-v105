b32a505bd0c5f52df9f297b634a56747909c9cca
jillzoe0301-gif <jillzoe0301@gmail.com>
2026-06-06 13:47:44 +0000

    simplify incident required fields

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"TCGRd1LLiGQveuxJdmzKfoPwdCQuelLOCSvLw1aXPUI=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"aQyijt9ahVVRrqveGp3qqjA7ytTUMzP8PiPNHheakDk=\"\n}"
