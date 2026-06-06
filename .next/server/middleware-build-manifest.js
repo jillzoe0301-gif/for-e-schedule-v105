d7feed914c778b060c356e6d4779b7e4b7f70345
jillzoe0301-gif <jillzoe0301@gmail.com>
2026-06-06 16:25:10 +0000

    adjust roles for translator and general staff

--- .next/server/middleware-build-manifest.js
+++ .next/server/middleware-build-manifest.js
@@ -7,9 +7,9 @@ globalThis.__BUILD_MANIFEST = {
     "static/chunks/0cz1d0mv5g_q7.js"
   ],
   "lowPriorityFiles": [
-    "static/4WeyRJU_hk1otGfsb31Dt/_buildManifest.js",
-    "static/4WeyRJU_hk1otGfsb31Dt/_ssgManifest.js",
-    "static/4WeyRJU_hk1otGfsb31Dt/_clientMiddlewareManifest.js"
+    "static/IyRWwsq3XfYsFcHuFuMRE/_buildManifest.js",
+    "static/IyRWwsq3XfYsFcHuFuMRE/_ssgManifest.js",
+    "static/IyRWwsq3XfYsFcHuFuMRE/_clientMiddlewareManifest.js"
   ],
   "rootMainFiles": [
     "static/chunks/3z5q_p4msz2ha.js",
--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"qaEEP05nC57Nx1T9xE4hAhMhdh5tDiKU++tYglt36+E=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"e61gj0AN8GGLQUIMrlGhMMMScy1aUEKoBHfCetaSuik=\"\n}"
